ff7c321fe1edb43fc072c5e23fac78e3f5eca0ed
Xavi <187180531+bcxavi@users.noreply.github.com>
2026-04-26 19:22:58 +0200

    feat: add computer choice function
    Create getComputerChoice to return random rock, paper or scissors

---
+++ script.js
@@ -0,0 +1,12 @@
+// I need a function that returns ´Rock´, ´Paper´or ´Scissors´randomly
+// Create a function that give me a random number 0 , 1 or 2
+// Associate the random number to a element in the game
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3)
+
+    if (num === 0) return 'Rock'
+    if (num === 1) return 'Paper'
+    return 'Scissors'
+}
+
